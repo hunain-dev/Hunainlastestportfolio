f181e60e8753caf83ae6c4688a13d3db3bcbd04c
huniiali1122@gmail.com <huniiali1122@gmail.com>
2025-11-25 00:18:30 +0500

    updated

--- app/sections/about/Aboutme.jsx
+++ app/sections/about/Aboutme.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 
 const Aboutme = () => {
   return (
-    <div className='min-h-full  bg-black flex items-center justify-start flex-col text-white w-full'>
-      <div className='h-[70vw] w-full bg-red-400 relative'>
+    <div className='min-h-full  flex items-center justify-start flex-col text-white w-full'>
+      <div className='h-[70vw] w-full relative'>
 
 
-        <div className='absolute top-0 left-0  flex items-center justify-center flex-col w-full bg-red-800'>
-      <h2 className='ppneuemontreal font-[600] flex items-center justify-center uppercase text-[21vw] leading-[4%]'>
+        <div className='absolute  top-0 left-0  flex items-center justify-center flex-col w-full '>
+      <h2 className='ppneuemontreal font-[600] flex items-center justify-center uppercase text-[24vw] leading-[4%]'>
         Huna 
         <span className="h-full   w-fit  ">
         <Image
     src="/assets/videos/hunainvideo-unscreen.gif"
-    width={130}
+    width={140}
     height={110}
     alt="Hunain GIF"
     className="object-cover"
@@ -21,7 +21,15 @@ const Aboutme = () => {
         </span>
         n
       </h2> 
-       <h4 className='actay leading-25 text-5xl'>Creative Developer</h4>
+      <h4 className="group text-8xl mt-5">
+  <span className="actay inline-block transition-transform duration-600 group-hover:-translate-x-50">
+    Creative
+  </span>
+  <span className="mx-3"></span>
+  <span className="actay inline-block transition-transform duration-600 group-hover:translate-x-50">
+    Developer
+  </span>
+</h4>
         </div>
       </div>
 

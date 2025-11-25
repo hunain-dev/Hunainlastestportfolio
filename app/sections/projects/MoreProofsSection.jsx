ee4ed6c05d9f47f890007bcac8dcd9765e39f724
huniiali1122@gmail.com <huniiali1122@gmail.com>
2025-11-25 19:20:41 +0500

    updated

---
+++ app/sections/projects/MoreProofsSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MoreProofsSection = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MoreProofsSection
--- app/sections/projects/Projects.jsx
+++ app/sections/projects/Projects.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import ProjectsShowcase from "../projects/ProjectsShowcase"
 const Projects = () => {
   return (
-    <div className='h-[100vh] w-full mt-10'>
+    <div className='h-full  w-full mt-10'>
        <div className="px-30 mx-auto mb-[1vw]">
       <h2 className="Satoshi font-medium text-[3vw] mb-4 text-center  text-[#242627]">
         Work Collection
@@ -11,6 +11,8 @@ const Projects = () => {
         Explore my collection of dynamic and engaging UI animations crafted using ReactJS, GSAP ScrollTrigger, and Locomotive Scroll. From smooth transitions to interactive experiences, each project showcases my expertise in bringing static designs to life with creativity, precision, and seamless motion.
       </h5>
     </div>
+
+    <ProjectsShowcase/>
     </div>
   )
 }
